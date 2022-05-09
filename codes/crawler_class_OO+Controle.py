22714a3e56ffd23d5fd7d411710e70c45df7432d
joaoriuto <jr00.dat@gmail.com>
2022-05-09 00:09:19 -0300

    Crawler com controle de conteúdo em OO

---
+++ codes/crawler_class_OO+Controle.py
@@ -0,0 +1,99 @@
+class Content:
+    #Classe para todos os artigos e páginas
+    
+    def __init__(self, topic, url, title, body):
+        self.topic = topic
+        self.url = url
+        self.title = title
+        self.body = body
+        
+        
+    def print(self):
+        #Exibe a saida e a controla
+        print("Novo artigo encontrado para este tópico: {}".format(self.topic))
+        print("Título: {}".format(self.title))
+        print("Conteúdo:\n{}".format(self.body))
+        print("Url: {}".format(self.url))
+        
+        
+class Website:
+    #Contém informações sobre a estrutura do site
+    
+    def __init__(self, name, url, searchUrl, resultListing, resultUrl, absoluteUrl, titleTag, bodyTag):
+        self.name = name
+        self.url = url
+        self.searchUrl = searchUrl
+        self.resultListing = resultListing
+        self.resultUrl = resultUrl
+        self.absoluteUrl = absoluteUrl
+        self.titleTag = titleTag
+        self.bodyTag = bodyTag
+        
+import requests
+from bs4 import BeautifulSoup
+
+class Crawler:
+    
+    def getPage(self, url):
+        try:
+            req = requests.get(url)
+        except requests.exceptions.RequestException:
+            return None
+        return BeautifulSoup(req.text, 'html.parser')
+    
+    def safeGet(self, pageObj, selector):
+        childObj = pageObj.select(selector)
+        if childObj is not None and len(childObj) > 0:
+            return childObj[0].get_text()
+        return ""
+        
+    def search(self, topic, site):
+        #Pesquisa um dado site em busca de um dado tópico 
+        #e registra todas as páginas encontradas
+        
+        bs = self.getPage(site.searchUrl + topic)
+        searchResults = bs.select(site.resultListing)
+        
+        for result in searchResults:
+            url = result.select(site.resultUrl)[0].attrs["href"]
+            #Verifica se é um Url relativo ou absoluto
+            if(site.absoluteUrl):
+                bs = self.getPage(url)
+            else:
+                bs = self.getPage(site.url + url)
+            if bs is None:
+                print("Algo errado não está certo nesta página ou Url. Vazando!")
+                return
+            
+            title = self.safeGet(bs, site.titleTag)
+            body = self.safeGet(bs, site.bodyTag)
+            if title != '' and body != '':
+                content = Content(topic, title, body, url)
+                content.print()
+                
+
+crawler = Crawler()
+
+siteData = [ ['O\'Reilly Media', 'http://oreilly.com', 'https://ssearch.oreilly.com/?q=','article.product-result', 
+             'p.title a', True, 'h1', 'section#product-description'], ['Reuters', 'http://reuters.com', 
+             'http://www.reuters.com/search/news?blob=',
+             'div.search-result-content','h3.search-result-title a',
+             False, 'h1', 'div.StandardArticleBody_body_1gnLA'], 
+             ['Brookings', 'http://www.brookings.edu',
+             'https://www.brookings.edu/search/?s=', 
+             'div.list-content article', 'h4.title a', True, 'h1', 'div.post-body'] ] 
+             
+sites = [] 
+             
+for row in siteData: 
+    sites.append(Website(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])) 
+            
+topics = ['python', 'data science'] 
+            
+for topic in topics: 
+    print("GETTING INFO ABOUT: " + topic)             
+    for targetSite in sites: 
+        crawler.search(topic, targetSite)
+
+
+
